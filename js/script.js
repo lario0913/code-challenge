83f7ba84b51e8dd52a79a5d5886a509eb9ebca7a
Lario <osunkunleolanrewaju@gmail.com>
2020-11-19 20:12:52 +0100

    I have been able to fetch the first 20 repositories from my github account using the graphql api

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
-// 7306caeaa1b796f3064253d271b1e4c56cf7f1d0
 const fetch = require("node-fetch") 
 const github_details ={
-    "token": "7306caeaa1b796f3064253d271b1e4c56cf7f1d0",
+    "token": "1ac9b28229943a76c8506f2cf8e8f758e3d53e5e",
     "username": "Lario0913"
 }
 
 
 const body = {
-    "query": `
-        query {
-            user(login: ${github_details["username"]}) {
-            repositories(first:20) {
-                nodes{
-                name
-                }
-                }
+    query: 
+    `query {
+        user(login: "lario0913" ) {
+        repositories(first:20) {
+            nodes{
+            name
+            }
             }
         }
-    `
+    }  `
 }
+
 
 const baseUrl = "https://api.github.com/graphql"
 
 const headers = {
     "Content-Type": "application/json",
-    Authentication: "bearer "+ github_details["token"]
+    Authorization: "token "+ github_details.token
 }
 
+require('isomorphic-fetch');
+
 fetch(baseUrl, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(body)
-}).
-then(response => {console.log(JSON.stringify(response))}).
-catch(error => console.log(JSON.stringify(error)))
+  method: 'POST',
+  headers: headers,
+  body: JSON.stringify(body)
+})
+  .then(res => res.json())
+  .then(res => console.log(res.data.user.repositories.nodes));
